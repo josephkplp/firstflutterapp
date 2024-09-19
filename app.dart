acba201516ef4a28933ab9b6ecbc58d4da4d6332
josephtheceo <159775398+josephkplp@users.noreply.github.com>
2024-09-19 05:40:06 +0000

    greating app

--- app.dart
+++ app.dart
@@ -1,1 +1,58 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'greeting app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('greating app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Text widget
+            Text(
+              'Welcome to My App!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20), // Spacing between widgets
+            // ElevatedButton widget
+            ElevatedButton(
+              onPressed: () {
+                print('Button clicked!');
+              },
+              child: Text('Click Me'),
+            ),
+            SizedBox(height: 20), // Spacing between widgets
+            // Image widget
+            Image.network(
+              'https://flutter.dev/images/flutter-logo-sharing.png',
+              width: 100,
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
